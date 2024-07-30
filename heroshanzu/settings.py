996e669374bf5571f2415456acea12f9a34e873e
heroshanzu <heroshanzu@gmail.com>
2024-07-30 18:02:20 +0300

    Adding message tags to contact submission

--- heroshanzu/settings.py
+++ heroshanzu/settings.py
@@ -140,8 +140,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER= config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_PASSWORD = 'uzsbcndafvdpneea'
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'heroshanzu@gmail.com'
 EMAIL_USE_TLS = True
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
